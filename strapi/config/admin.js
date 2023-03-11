a69c54d1c9c1f7b900cd7027ee1268fcbae449c1
Boris Syrpin <betelgeusexru@gmail.com>
2023-03-12 02:53:00 +0500

    update env

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET", "sYa/U3VV4trIT1E6tVuUWA=="),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT", "yvmkgRfHTpKfAsETrBGvxA=="),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT", "J4dYpDxEEzz13IvobPLdww=="),
     },
   },
 });
--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array(
+      "APP_KEYS",
+      "G9J76rLMvKhwhQ4nfJ3sog==,GzeB3RUp+PdEY4/8K/V9dw==,FTHoFol5LLpw3/La3/EY4g==,8ggsv90w+jQl/EUSXUoCAQ=="
+    ),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
