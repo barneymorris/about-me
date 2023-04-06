52f6e5710df2f785024484d55f823ace05319700
bsyrpin <bsyrpin@moysklad.ru>
2023-04-06 20:48:07 +0500

    smth

--- strapi/build/transfer-tokens-create-page.924233ce.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkstrapi=self.webpackChunkstrapi||[]).push([[4816],{39987:(a,t,e)=>{e.r(t),e.d(t,{default:()=>_});var s=e(67294),n=e(95489),P=e.n(n),E=e(87751),r=e(53219);const _=()=>s.createElement(n.CheckPagePermissions,{permissions:E.Z.settings["transfer-tokens"].create},s.createElement(r.Z,null))}}]);
